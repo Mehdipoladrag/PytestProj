e2ac8183a85fb75d51e9ce21cb0e15f2b5acfe4e
Mehdi Poladrag <mehdipoladrag1382@gmail.com>
2025-12-19 18:58:22 +0330

    (feat): Create a test for Login Page

---
+++ users/tests/test_login.py
@@ -0,0 +1,43 @@
+import pytest
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+class TestLogin:
+    def test_login_page_loads(self, client):
+        url = reverse("users:login")
+        response = client.get(url)
+
+        assert response.status_code == 200
+        assert b"Login" in response.content
+
+    def test_login_success(self, client, django_user_model):
+        user = django_user_model.objects.create_user(
+            username="testuser",
+            password="StrongPass123"
+        )
+
+        url = reverse("users:login")
+        response = client.post(url, {
+            "username": "testuser",
+            "password": "StrongPass123",
+        })
+
+        assert response.status_code == 302
+        assert response.url == reverse("homepage")
+        assert "_auth_user_id" in client.session
+
+    def test_login_wrong_password(self, client, django_user_model):
+        django_user_model.objects.create_user(
+            username="testuser",
+            password="StrongPass123"
+        )
+
+        url = reverse("users:login")
+        response = client.post(url, {
+            "username": "testuser",
+            "password": "WrongPass123",
+        })
+
+        assert response.status_code == 200
+        assert b"invalid" in response.content.lower()
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login
+from django.contrib.auth.views import LoginView as DjangoLoginView, LogoutView as DjangoLogoutView
+
 from .forms import SignupForm
 from .models import User
 
@@ -15,3 +17,16 @@ class SignupView(CreateView):
         response = super().form_valid(form)
         login(self.request, self.object)
         return response
+
+
+class LoginView(DjangoLoginView):
+    template_name = "users/login.html"
+    redirect_authenticated_user = True
+    success_url = reverse_lazy("homepage")
+
+    def get_success_url(self):
+        return self.success_url
+
+
+class LogoutView(DjangoLogoutView):
+    next_page = reverse_lazy("users:login")
